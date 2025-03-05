a83626d67552bcffa1a48b22ab5b8e0affbb5a32
Jinju Yu <34618639+jinju-yu@users.noreply.github.com>
2025-03-05 20:55:08 +0900

    [Bronze IV] Title: 영수증, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,17 @@
+import sys
+
+if __name__ == "__main__":
+    all_price = int(input())
+    all_num = int(input())
+    prices = [list(sys.stdin.readline().split(' ')) for _ in range(all_num)]
+
+    sum_price = 0
+    for info in prices:
+        price = int(info[0])
+        num = int(info[1].split('\n')[0])
+        sum_price += price * num
+
+    if  all_price == sum_price:
+        print('Yes')
+    else:
+        print('No')
